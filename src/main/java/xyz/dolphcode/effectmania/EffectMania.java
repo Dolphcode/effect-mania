bb0f74e2eec9148497a7282a917e779321724fa4
InfernoProgrammer123 <bastivargas@gmail.com>
2021-04-11 19:15:38 -0400

    Initial commit

---
+++ src/main/java/xyz/dolphcode/effectmania/EffectMania.java
@@ -0,0 +1,10 @@
+package xyz.dolphcode.effectmania;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EffectMania implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/xyz/dolphcode/effectmania/client/EffectManiaClient.java
@@ -0,0 +1,13 @@
+package xyz.dolphcode.effectmania.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class EffectManiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
