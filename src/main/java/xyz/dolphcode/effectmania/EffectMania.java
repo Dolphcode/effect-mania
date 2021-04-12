e304bf21589fbda21b13f3bdae9036fdc54eb491
InfernoProgrammer123 <bastivargas@gmail.com>
2021-04-12 12:32:36 -0400

    Added spiral effect

--- src/main/java/xyz/dolphcode/effectmania/EffectMania.java
+++ src/main/java/xyz/dolphcode/effectmania/EffectMania.java
@@ -3,6 +3,9 @@ package xyz.dolphcode.effectmania;
 import net.fabricmc.api.ModInitializer;
 
 public class EffectMania implements ModInitializer {
+
+    public static final String ID = "effect_mania";
+
     @Override
     public void onInitialize() {
 
--- src/main/java/xyz/dolphcode/effectmania/screen/RainbowOverlayRenderer.java
+++ src/main/java/xyz/dolphcode/effectmania/screen/RainbowOverlayRenderer.java
@@ -5,9 +5,14 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
+import org.lwjgl.system.CallbackI;
+import xyz.dolphcode.effectmania.EffectMania;
 
 import java.awt.*;
 import java.util.logging.Logger;
@@ -19,6 +24,7 @@ public class RainbowOverlayRenderer {
     public static int B = 0;
     public static int COUNTER = 255;
     public static int COLOR_CHANGE = 0;
+    public static int FRAME = 1;
 
     public static void renderRainbowOverlay(MatrixStack matrixStack, float tickDelta) {
         MinecraftClient client = MinecraftClient.getInstance();
@@ -27,6 +33,7 @@ public class RainbowOverlayRenderer {
             matrixStack.push();
             matrixStack.translate(0, 0, 0);
 
+            // Change color values every tick
             switch(COLOR_CHANGE) {
                 case 0:
                     G += 1;
@@ -56,10 +63,23 @@ public class RainbowOverlayRenderer {
                 }
             }
 
-            String colorHex = "96" + String.format("%02X", R) + String.format("%02X", G) + String.format("%02X", B);
-            System.out.println(colorHex);
+            // Change frame every tick
+            FRAME++;
+            if (FRAME > 12) {
+                FRAME = 1;
+            }
+
+            int sWidth = (int) (client.getWindow().getScaledWidth() * 1.2);
+            int sHeight = client.getWindow().getScaledHeight();
+
+            Identifier id = new Identifier(EffectMania.ID, "textures/gui/spiral.png");
+            client.getTextureManager().bindTexture(id);
+            DrawableHelper.drawTexture(matrixStack, 0, (sHeight/2) - (sWidth/2), sWidth, sWidth, 0, sWidth * (FRAME - 1), sWidth, sWidth, sWidth, sWidth * 12);
+
+            String colorHex = "25" + String.format("%02X", R) + String.format("%02X", G) + String.format("%02X", B);
             int color = (int) Long.parseLong(colorHex, 16);
-            DrawableHelper.fill(matrixStack, 0, 0, client.getWindow().getScaledWidth(), client.getWindow().getScaledHeight(), color);
+            DrawableHelper.fill(matrixStack, 0, 0, sWidth, sHeight, color);
+
             matrixStack.pop();
         }
     }
